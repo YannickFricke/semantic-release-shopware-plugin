127ffb95433d1a85b7879d236af5ea774198acca
Yannick Fricke <yannickfricke@googlemail.com>
2019-06-26 19:59:43 +0200

    refactor(IPluginConfig): Added the basic definition for the plugin configuration

---
+++ src/config/IPluginConfig.ts
@@ -0,0 +1,20 @@
+/**
+ * Contains all fields for the plugin configuration
+ */
+export interface IPluginConfig {
+    /**
+     * Contains the URL to the Shopware Comunity Store
+     */
+    endpoint: string | undefined;
+
+    /**
+     * The file system which should be used
+     */
+    fileSystem: string | undefined;
+
+    /**
+     * When set to true, then version in the plugin.xml
+     * will be updated to the newest version which was determined by semantic-release
+     */
+    shouldIncrementVersion: boolean;
+}
