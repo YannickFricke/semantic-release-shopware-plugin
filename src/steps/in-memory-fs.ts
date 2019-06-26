4c77310bbcab17097176f8f2934d2ee5fdc3859c
Yannick Fricke <yannickfricke@googlemail.com>
2019-06-26 20:20:39 +0200

    test(InMemoryFS): Added an in memory filesystem for the tests

---
+++ src/steps/in-memory-fs.ts
@@ -0,0 +1,24 @@
+import { readFileSync } from "fs";
+import { resolve } from "path";
+
+const pluginXmlPath = resolve(
+    process.cwd(),
+    'plugin.xml'
+);
+
+const fileSystem: any = {};
+
+fileSystem[pluginXmlPath] = readFileSync(
+    resolve(
+        __dirname,
+        '..',
+        '__mocks__',
+        'plugin.xml'
+    )
+);
+
+module.exports = {
+    existsSync: (fileName: string) => fileSystem[fileName] !== undefined,
+    readFileSync: (fileName: string) => fileSystem[fileName],
+    writeFileSync: (fileName: string, content: string) => fileSystem[fileName] = content,
+};
