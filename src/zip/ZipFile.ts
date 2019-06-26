84fa9398be737ee74284d44f7d4e207f4af58fb3
Yannick Fricke <yannickfricke@googlemail.com>
2019-06-26 22:44:13 +0200

    feat(ZipFile): Added the basic ZipFile class
    This class packs a new archive based on the files in the directory. It walks over the file system
    recursively to include every folder and every file.

---
+++ src/zip/ZipFile.ts
@@ -0,0 +1,75 @@
+import { create } from "archiver";
+import { resolve } from "path";
+import { createWriteStream, createReadStream, readdirSync, statSync } from "fs";
+
+export class ZipFile {
+    private _rootDirectory: string;
+
+    constructor(
+        rootDirectory: string = process.cwd(),
+    ) {
+        this._rootDirectory = rootDirectory;
+    }
+
+    public get rootDirectory(): string {
+        return this._rootDirectory;
+    }
+
+    public generateFileList(directory: string = ''): string[] {
+        const result: string[] = [];
+
+        if (directory === '') {
+            directory = this._rootDirectory;
+        }
+
+        const directoryContents = readdirSync(directory);
+
+        directoryContents.forEach(entry => {
+            const completePath = `${directory}/${entry}`;
+
+            const entryStats = statSync(completePath);
+
+            if (entryStats.isDirectory()) {
+                result.push(
+                    ...this.generateFileList(completePath)
+                );
+            } else {
+                result.push(completePath);
+            }
+        });
+
+        return result;
+    }
+
+    public determineFolderName() {
+        let result = this._rootDirectory;
+
+        if (this._rootDirectory.includes('/')) {
+            const parts = this._rootDirectory.split('\/');
+
+            result = parts[parts.length - 1];
+        }
+
+        return result;
+    }
+
+    public packZip(newVersion: string) {
+        const folderName = this.determineFolderName();
+        const resultFileName = createWriteStream(`${folderName}-${newVersion}.zip`);
+        const zipFile = create('zip');
+
+        const filesToArchive = this.generateFileList();
+
+        filesToArchive.forEach(file => {
+            zipFile.append(createReadStream(file), {
+                name: file.substr(this._rootDirectory.length + 1),
+            });
+        });
+
+        zipFile.pipe(resultFileName);
+        zipFile.finalize();
+
+        return zipFile;
+    }
+}
+
