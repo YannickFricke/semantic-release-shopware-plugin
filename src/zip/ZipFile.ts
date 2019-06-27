38f83fa2b185dd8722e368fdea2fbc8190d18cd8
Yannick Fricke <yfr@mts.de>
2019-06-27 13:43:10 +0200

    docs(ZipFile): Added jsdocs for the ZipFile class

--- src/zip/ZipFile.ts
+++ src/zip/ZipFile.ts
@@ -2,19 +2,39 @@ import { create } from "archiver";
 import { sep } from "path";
 import { createWriteStream, createReadStream, readdirSync, statSync } from "fs";
 
+/**
+ * Zip file
+ *
+ * @class
+ * @author Yannick Fricke <https://github.com/YannickFricke>
+ */
 export class ZipFile {
+    /**
+     * Root directory of zip file
+     */
     private _rootDirectory: string;
 
+    /**
+     * Creates an instance of zip file.
+     * @param [rootDirectory] The base directory
+     */
     constructor(
         rootDirectory: string = process.cwd(),
     ) {
         this._rootDirectory = rootDirectory;
     }
 
+    /**
+     * Gets root directory
+     */
     public get rootDirectory(): string {
         return this._rootDirectory;
     }
 
+    /**
+     * Generates a file list based of the given directory
+     * The root directory will be used when the given directory is an empty string
+     */
     public generateFileList(directory: string = ''): string[] {
         const result: string[] = [];
 
@@ -41,7 +61,12 @@ export class ZipFile {
         return result;
     }
 
-    public determineFolderName() {
+    /**
+     * Returns the directory name of the root directory
+     *
+     * @returns {string} The directory name
+     */
+    public determineFolderName(): string {
         let result = this._rootDirectory;
 
         if (result.includes(sep)) {
@@ -53,6 +78,12 @@ export class ZipFile {
         return result;
     }
 
+    /**
+     * Creates a zip archive with the following pattern:
+     * $NAME-$VERSION.zip
+     *
+     * @param newVersion The new version of the plugin
+     */
     public packZip(newVersion: string) {
         const folderName = this.determineFolderName();
         const resultFileName = createWriteStream(`${folderName}-${newVersion}.zip`);
